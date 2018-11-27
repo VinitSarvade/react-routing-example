f15eba15d68b31a161d5d3cb496dcfb37ff3933c
Vinit Sarvade <vinit.sarvade@furlenco.com>
2018-11-27 15:36:37 +0530

    Routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Switch, Route } from 'react-router';
+
+import Home from './components/home';
+import NewPage from './components/new-page';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route path='/new' component={NewPage} />
+          <Route path='' component={Home} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
